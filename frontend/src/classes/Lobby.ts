565c9af7e959310d8252f0d8a90075abb96b96cb
JackGitter <gitter.j@northeastern.edu>
2023-12-01 18:29:16 -0500

    sorted order, nice json

--- frontend/src/classes/Lobby.ts
+++ frontend/src/classes/Lobby.ts
@@ -2,61 +2,67 @@ import { EventEmitter } from "events";
 import Room from "./Room";
 
 export default class Lobby extends EventEmitter {
-    public rooms: Room[]
-    public otherPlayers: string[]
-    public ourPlayerID: string
-    public ourPlayerSocket: WebSocket | undefined
+  public rooms: Room[];
+  public otherPlayers: string[];
+  public ourPlayerID: string;
+  public ourPlayerSocket: WebSocket | undefined;
 
-    constructor() {
-        super()
-        this.rooms = []
-        this.otherPlayers = []
-        this.ourPlayerID = ""
-        this.ourPlayerSocket = undefined
+  constructor() {
+    super();
+    this.rooms = [];
+    this.otherPlayers = [];
+    this.ourPlayerID = "";
+    this.ourPlayerSocket = undefined;
+  }
+
+  private handleConnectionResponse(jsonMessage: any) {
+    this.ourPlayerID = jsonMessage.OurPlayerID;
+
+    for (const playerID in jsonMessage.Lobby.PlayersInLobby) {
+      this.otherPlayers.push(playerID);
     }
+    this.rooms = jsonMessage.Rooms;
+    this.emit("connectionResponse", this);
+  }
 
-    public async addUserToLobby() {
+  private handleLobbyUpdate(jsonMessage: any) {
+    for (const playerID in jsonMessage.Lobby.PlayersInLobby) {
+      if (
+        playerID !== this.ourPlayerID &&
+        this.otherPlayers.find((pID) => pID === playerID) === undefined
+      ) {
+        this.otherPlayers.push(playerID);
+      }
+      this.otherPlayers = this.otherPlayers.sort();
+    }
+    this.rooms = jsonMessage.Rooms;
+    this.emit("LobbyUpdate", this);
+  }
 
-        this.ourPlayerSocket = new WebSocket("ws://localhost:8080/lobby")
-        
-        // here i need to get the lobby from the server -- just send a message to get the lobby state
-        this.ourPlayerSocket.onmessage = (event) => {
-            try {
-                let jsonMessage = JSON.parse(event.data)
-                if (jsonMessage["CmdType"] === "connectionResponse") {
-                    this.ourPlayerID = jsonMessage["OurPlayerID"]
-                    for (const playerID in jsonMessage["Lobby"]["PlayersInLobby"]) {
-                       this.otherPlayers.push(playerID)
-                    }
-                    this.rooms = jsonMessage["Rooms"]
-                    this.emit("connectionResponse", this)
-                } 
-                if (jsonMessage["CmdType"] === "LobbyUpdate") {
-                    console.log("got this command")
-                    for (const playerID in jsonMessage["Lobby"]["PlayersInLobby"]) {
-                        if (playerID !== this.ourPlayerID && this.otherPlayers.find((pID) => pID === playerID) === undefined) {
-                            this.otherPlayers.push(playerID)
-                        }
-                        
-                    }
-                    this.rooms = jsonMessage["Rooms"]
-                    this.emit("LobbyUpdate", this)
-                }
-            } catch (err) {
-                if (err instanceof Error) {
-                    console.log(err)
-                }
-            }
-            
+  public async addUserToLobby() {
+    this.ourPlayerSocket = new WebSocket("ws://localhost:8080/lobby");
+
+    this.ourPlayerSocket.onmessage = (event) => {
+      try {
+        let jsonMessage = JSON.parse(event.data);
+        let cmdType = jsonMessage.CmdType;
+        if (cmdType === "connectionResponse") {
+          this.handleConnectionResponse(jsonMessage);
         }
+        if (cmdType === "LobbyUpdate") {
+          this.handleLobbyUpdate(jsonMessage);
+        }
+      } catch (err) {
+        if (err instanceof Error) {
+          console.log(err);
+        }
+      }
+    };
 
-        this.ourPlayerSocket.onopen = () => this.ourPlayerSocket?.send(JSON.stringify({"cmdType": "connect"}))
-        
-        
-        
-    }
-
+    this.ourPlayerSocket.onopen = () =>
+      this.ourPlayerSocket?.send(JSON.stringify({ cmdType: "connect" }));
+  }
 }
 
-// player joins lobby, we create a socket connection for them 
+// player joins lobby, we create a socket connection for them
 // we set up listeners on that socket connection to update the lobby and then the lobby emits events
