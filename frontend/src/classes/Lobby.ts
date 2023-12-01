48be7b39e684f58ca873e35e3a6e35d4b17f0513
JackGitter <gitter.j@northeastern.edu>
2023-12-01 13:53:51 -0500

    working on adding players to lobby from frontend

--- frontend/src/classes/Lobby.ts
+++ frontend/src/classes/Lobby.ts
@@ -4,11 +4,25 @@ import Room from "./Room";
 export default class Lobby extends EventEmitter {
     public rooms: Room[]
     public playerConnections: Map<string, WebSocket>
+    public ourPlayerID: string
 
     constructor() {
         super()
         this.rooms = []
         this.playerConnections = new Map()
+        this.ourPlayerID = ""
+    }
+
+    public addUserToLobby() {
+        // create a new websocket for the user
+        // add the user to the backend model 
+        // get the backend model
+        // update frontend model
+        let socket = new WebSocket("ws://localhost:8080/lobby")
+        // check for the userID sent back from the server 
+        socket.onmessage = (event) => {this.ourPlayerID = event.data}
+        this.playerConnections.set(this.ourPlayerID, socket)
+        
     }
 
 }
