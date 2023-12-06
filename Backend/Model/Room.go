11899ee81071f2a202489ed6db85cb1612b5bb3f
JackGitter <gitter.j@northeastern.edu>
2023-12-05 19:12:30 -0500

    working on making passwords for rooms now

--- Backend/Model/Room.go
+++ Backend/Model/Room.go
@@ -9,4 +9,5 @@ type Room struct {
 	PlayerConnections map[string]*websocket.Conn
 	Messages          [][]string // of the form [[username: message], [username: message], [username, message]] in order
 	Owner             string     // the playerID of the user who created the room
+	Password          string
 }
